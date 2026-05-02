540818a2271a4776b988aec3e8657038d3d07916
Emmanuel Munene <emmanuel.munene@student.moringaschool.com>
2026-05-02 11:45:20 +0300

    adding vite config with react plugin and vitest setup

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
+import {defineconfig} from 'vite';
+import react from "@vitejs/plugin-react";
 
+export default defineconfig({
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.js',
+    },
+});
